2cd2ede254a31723c85a3f4a3c488f8969cce711
ilorde-exe <92220271+ilorde-exe@users.noreply.github.com>
2024-05-18 00:41:54 +0530

    improvementz

--- 509fibonacci.cpp
+++ 509fibonacci.cpp
@@ -5,14 +5,21 @@ using namespace std;
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1){
-            return n ;
+        int t=0,t1=1,next;
+        next=t1+t;
+        for(int i=3;i<=n;i++){
+            t=t1;
+            t1=next;
+            next=t+t1;
+            
         }
-        return fib(n-1) + fib(n-2);
+        if(n==0) return t;
+        if(n==1) return t1;
+        return next;
     }
 };
 int main(){
     Solution sol;
-    cout << sol.fib(4) << endl;
+    cout << sol.fib(2) << endl;
     return 0;
 }
