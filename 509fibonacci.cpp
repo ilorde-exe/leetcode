55a866716b494d5bd59bbcc4fb525109099acc0a
ilorde-exe <92220271+ilorde-exe@users.noreply.github.com>
2024-05-18 00:23:32 +0530

    fibnacii

---
+++ 509fibonacci.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1){
+            return n ;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+};
+int main(){
+    Solution sol;
+    cout << sol.fib(4) << endl;
+    return 0;
+}
